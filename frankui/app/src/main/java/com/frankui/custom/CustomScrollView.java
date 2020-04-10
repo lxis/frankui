5886e884b1f1d19b4aa249ee72590af5719cd6f8
lixiang34 <lixiang34@baidu.com>
2020-04-10 18:27:42 +0800

    frankui多状态列表迁移-step12

--- frankui/app/src/main/java/com/frankui/custom/CustomScrollView.java
+++ frankui/app/src/main/java/com/frankui/custom/CustomScrollView.java
@@ -1,42 +1,29 @@
 package com.frankui.custom;
 
 import android.content.Context;
-import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
-import android.view.ViewConfiguration;
-import android.webkit.WebView;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.ScrollView;
-import android.widget.Scroller;
 
 
 import com.frankui.frankui.R;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
+// 两状态列表
 public class CustomScrollView extends ScrollView {
 
-    private VelocityTracker mVelocityTracker;
-    private int mMaxVelocity;
-
-    private static final int TWO_LEVEL_VEL = 1000;
-
-    private static final int VELOCTITY = 1000;
-
-    // 三种状态scrollto的位置
-    public int bottom;
-    public int top;
-
-    private LinearLayout blankLayout;
-    public LinearLayout contentLayout;
-
+    // 顶部状态
+    private int mTop;
+    // 底部状态
+    private int mBottom;
+    // 内容布局，包含顶部空白和下面内容
+    private LinearLayout mContentLayout;
+    // 内容顶部插入空白控制状态
+    private FrameLayout mBlankLayout;
+    // 当前状态
     private PageScrollStatus mStatus = PageScrollStatus.BOTTOM;
 
     public CustomScrollView(Context context) {
@@ -54,104 +41,51 @@ public class CustomScrollView extends ScrollView {
     }
 
     public void initView(Context context) {
-        bottom = 0;
         LayoutInflater.from(context).inflate(R.layout.custom_scroll_view, this);
-        blankLayout = findViewById(R.id.ll_blank);
-        contentLayout = findViewById(R.id.ll_content);
-        mMaxVelocity = ViewConfiguration.get(getContext()).getScaledMaximumFlingVelocity();
+        mContentLayout = findViewById(R.id.ll_content);
+        mBlankLayout = findViewById(R.id.ll_blank);
+        setVerticalScrollBarEnabled(false); // 隐藏右侧的ScrollBar
     }
 
     public void setStatusHeight(int top, int bottom) {
-        this.top = top;
-        this.bottom = bottom;
-
-        setVerticalScrollBarEnabled(false);
+        mTop = top;
+        mBottom = bottom;
     }
 
     public void setBlankHeight(int height) {
-        blankLayout.getLayoutParams().height = height;
-        blankLayout.setLayoutParams(blankLayout.getLayoutParams());
+        mBlankLayout.getLayoutParams().height = height;
+        mBlankLayout.setLayoutParams(mBlankLayout.getLayoutParams());
     }
 
     public void addContentView(View view) {
-        contentLayout.addView(view);
+        mContentLayout.addView(view);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        acquireVelocityTracker(ev);
         if (ev.getAction() == MotionEvent.ACTION_UP) {
-            final VelocityTracker verTracker = mVelocityTracker;
-            if (verTracker != null) {
-                verTracker.computeCurrentVelocity(VELOCTITY, mMaxVelocity);
-                int initialVelocity = (int) verTracker.getYVelocity();
-                PageScrollStatus status = calculateNextStatusWhenTwo(initialVelocity, getScrollY());
-                if (status != PageScrollStatus.NULL) {
-                    updateStatus(status, true);
-                    return true;
-                }
-                mStatus = status;
+            PageScrollStatus status = calculateNextStatus(getScrollY());
+            mStatus = status;
+            if (status != PageScrollStatus.NULL) {
+                updateStatus(status, true);
+                return true;
             }
         }
         return super.onTouchEvent(ev);
     }
 
-    private PageScrollStatus calculateNextStatusWhenTwo(int initialVelocity, int scrollY) {
-        switch (mStatus) {
-            case BOTTOM:
-                if (scrollY > top) {
-                    return PageScrollStatus.NULL;
-                } else if ((initialVelocity < 0 && Math.abs(initialVelocity) > TWO_LEVEL_VEL) || scrollY > getMid()) {
-                    return PageScrollStatus.TOP;
-                } else {
-                    return PageScrollStatus.BOTTOM;
-                }
-            case TOP:
-                if (scrollY < getMid()) {
-                    return PageScrollStatus.BOTTOM;
-                } else if (scrollY < getMid()) {
-                    return PageScrollStatus.BOTTOM;
-                } else if (scrollY > top) {
-                    return PageScrollStatus.NULL;
-                } else {
-                    return PageScrollStatus.TOP;
-                }
-            case NULL:
-                if (initialVelocity > TWO_LEVEL_VEL) {
-                    return PageScrollStatus.TOP;
-                } else if (scrollY > top) {
-                    return PageScrollStatus.TOP;
-                } else if (scrollY > getMid()) {
-                    return PageScrollStatus.TOP;
-                } else if (scrollY > getMid()) {
-                    return PageScrollStatus.TOP;
-                } else {
-                    return PageScrollStatus.BOTTOM;
-                }
-            default:
-                return PageScrollStatus.BOTTOM;
+    private PageScrollStatus calculateNextStatus(int scrollY) {
+        if (scrollY > getMid()) {
+            return PageScrollStatus.TOP;
+        } else {
+            return PageScrollStatus.BOTTOM;
         }
     }
 
     private int getMid() {
-        return (top + bottom) / 2;
+        return (mTop + mBottom) / 2;
     }
 
-    private void acquireVelocityTracker(final MotionEvent event) {
-        if (null == mVelocityTracker) {
-            mVelocityTracker = VelocityTracker.obtain();
-        }
-        mVelocityTracker.addMovement(event);
-    }
-
-    @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        if (mVelocityTracker != null) {
-            mVelocityTracker.recycle();
-            mVelocityTracker = null;
-        }
-    }
 
     /**
      * 设置状态，不会回调
@@ -173,29 +107,20 @@ public class CustomScrollView extends ScrollView {
         switch (status) {
             case BOTTOM:
                 if (smooth) {
-                    smoothScrollTo(0, bottom);
+                    smoothScrollTo(0, mBottom);
                 } else {
-                    scrollTo(0, bottom);
+                    scrollTo(0, mBottom);
                 }
                 break;
             case TOP:
                 if (smooth) {
-                    smoothScrollTo(0, top);
+                    smoothScrollTo(0, mTop);
                 } else {
-                    scrollTo(0, top);
+                    scrollTo(0, mTop);
                 }
                 break;
             default:
                 break;
         }
-    }
-
-    @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        // lxis：是为了处理当scroll高于top时自动回弹回top
-        if (t > top) {
-            setStatus(PageScrollStatus.NULL);
-        }
-        super.onScrollChanged(l, t, oldl, oldt);
     }
 }
